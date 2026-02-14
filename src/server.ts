3978c6073d080d633b3d06a40b42a38e2903511d
Lungelo <lungelomkhwemte11@gmail.com>
2026-02-14 21:59:28 +0200

    Cancel admin routes

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Import Routes
-import adminRoutes from './routes/admin.route';
+// import adminRoutes from './routes/admin.route';
 import authRoutes from './routes/auth.routes';
 
 // Middleware
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use('/api', adminRoutes);
+// app.use('/api', adminRoutes);
 app.use ('/api', authRoutes);
 
 
