234778da0422a5b1d73f641c876ef91ef8f830b7
Lungelo <lungelomkhwemte11@gmail.com>
2026-02-14 23:36:25 +0200

    Prisma import fixes2

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 import { PrismaNeon } from '@prisma/adapter-neon'
-import { PrismaClient } from '../generated/prisma/edge.js'
+import { PrismaClient } from "@prisma/client";
 // use `prisma` in your application to read and write data in your DB// use `prisma` in your application to read and write data in your DB
 const adapter = new PrismaNeon({
   connectionString: process.env.DATABASE_URL!,
